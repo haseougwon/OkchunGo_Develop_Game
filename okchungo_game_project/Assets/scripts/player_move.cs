e2babd3dc158f86740da4783b2bd931cdc3d8478
haseougwon <kid03181@gmail.com>
2023-09-24 17:17:20 +0900

    플레이어 애니메이션

--- okchungo_game_project/Assets/scripts/player_move.cs
+++ okchungo_game_project/Assets/scripts/player_move.cs
@@ -2,26 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player_move : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
+    public float jumpPower;
     Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        rigid.freezeRotation = true;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
+    
+    private void Update()
+    {
+        if (Input.GetButtonUp("Horizontal"))
+        {
+
+            rigid.velocity = new Vector2(0, rigid.velocity.y);
+        }
+     
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            
+            spriteRenderer.flipX = (Input.GetAxisRaw("Horizontal") == -1);
+        }
+
+        if (Input.GetAxisRaw("Horizontal") == 0)
+        {
+            anim.SetBool("isRun", false);
+            anim.SetTrigger("Standing");
+
+        }
+        else
+        {
+            anim.SetBool("isRun", false);
+            anim.SetBool("isRun", true);
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+
+        }
+
+    }
     void FixedUpdate()
     {
-        //ĳ���� ������
         float h = Input.GetAxisRaw("Horizontal");
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
-
-        if(rigid.velocity.x > maxSpeed)  //������ �ִ�ӵ�
+        if (rigid.velocity.x > maxSpeed)
         {
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
         }
-        else if (rigid.velocity.x < maxSpeed * (-1)) //���� �ִ�ӵ�
+        else if (rigid.velocity.x < maxSpeed * (-1))
         {
             rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
         }
