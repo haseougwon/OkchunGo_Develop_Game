14d3989a6f462b4dc77773969e40354268b2fae2
haseougwon <kid03181@gmail.com>
2023-11-05 21:05:36 +0900

    점프 애니메이션, 스테이지 포탈

--- okchungo_game_project/Assets/scripts/player_move.cs
+++ okchungo_game_project/Assets/scripts/player_move.cs
@@ -17,7 +17,7 @@ public class PlayerMove : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    
+
     private void Update()
     {
         if (Input.GetButtonUp("Horizontal"))
@@ -25,42 +25,65 @@ public class PlayerMove : MonoBehaviour
 
             rigid.velocity = new Vector2(0, rigid.velocity.y);
         }
-     
+
         if (Input.GetButtonDown("Horizontal"))
         {
-            
+
             spriteRenderer.flipX = (Input.GetAxisRaw("Horizontal") == -1);
         }
 
         if (Input.GetAxisRaw("Horizontal") == 0)
         {
-            anim.SetBool("isRun", false);
-            anim.SetTrigger("Standing");
-
+            if (Input.GetButtonDown("Jump"))
+            {
+                anim.SetBool("isRun", false);
+                anim.SetBool("isJump", true);
+                rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            }
+            else
+            {
+                anim.SetBool("isRun", false);
+                anim.SetBool("isJump", false);
+            }
         }
         else
         {
-            anim.SetBool("isRun", false);
-            anim.SetBool("isRun", true);
-        }
-        if (Input.GetButtonDown("Jump"))
-        {
-            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            if (Input.GetButtonDown("Jump"))
+            {
+                anim.SetBool("isRun", false);
+                anim.SetBool("isJump", true);
+                rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            }
+            else
+            {
+                anim.SetBool("isJump", false);
+            }
+            //if (Input.GetButtonDown("Jump"))
+            //{
+            //    anim.SetBool("isRun", false);
+            //    anim.SetBool("isJump", true);
+            //    rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            //}
+            //else
+            //{
+            //    anim.SetBool("isJump", false);
+
+            //}
 
         }
-
     }
-    void FixedUpdate()
-    {
-        float h = Input.GetAxisRaw("Horizontal");
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
-        if (rigid.velocity.x > maxSpeed)
+        void FixedUpdate()
         {
-            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+            float h = Input.GetAxisRaw("Horizontal");
+            rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+            if (rigid.velocity.x > maxSpeed)
+            {
+                rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+            }
+            else if (rigid.velocity.x < maxSpeed * (-1))
+            {
+                rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+            }
         }
-        else if (rigid.velocity.x < maxSpeed * (-1))
-        {
-            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
-        }
+   
     }
-}
