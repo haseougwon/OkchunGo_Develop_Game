ddf214422114041ba23fc5d52b3172bdd2765890
yejin1122 <yejin6922@gmail.com>
2023-11-05 21:31:44 +0900

    카메라 고정&세이브 로드

---
+++ okchungo_game_project/Assets/scripts/GameData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public class GameData
+{
+    public int coin;
+    public int heart;
+    public float timeElaspsed;
+}
